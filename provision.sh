cd728919cfff822e46b4b660a2e712fb7dfc22b2
Taro YABUKI <taro.yabuki@it-chiba.ac.jp>
2016-05-17 01:37:17 +0900

    MySQLへのホストからの接続，PHPのエラー表示

--- provision.sh
+++ provision.sh
@@ -12,12 +12,19 @@ echo "mysql-server mysql-server/root_password password $MYSQL_ROOT_PASS" | debco
 echo "mysql-server mysql-server/root_password_again password $MYSQL_ROOT_PASS" | debconf-set-selections
 apt-get -y install mysql-server
 
+#to access MySQL from host
+sed -i -e 's/^bind-address.*/bind-address = 0.0.0.0/' /etc/mysql/my.cnf
+service mysql restart
+
 #phpMyAdmin
 echo "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2" | debconf-set-selections
 echo "phpmyadmin phpmyadmin/dbconfig-install boolean true" | debconf-set-selections
 echo "phpmyadmin phpmyadmin/mysql/admin-pass password $MYSQL_ROOT_PASS" | debconf-set-selections
 echo "phpmyadmin phpmyadmin/mysql/app-pass password ''" | debconf-set-selections
 apt-get -y install phpmyadmin
+
+#display PHP errors
+sed -i -e 's/display_errors = Off/display_errors = On/' /etc/php5/apache2/php.ini
 
 #Others
 apt-get -y install git jq
