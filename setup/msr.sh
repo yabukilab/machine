7d47ac10df66454a887b0dd8aca93fd94161b359
Taro YABUKI <taro.yabuki@it-chiba.ac.jp>
2018-01-22 21:39:48 +0900

    MS R Open 3.4.3

--- setup/msr.sh
+++ setup/msr.sh
@@ -4,14 +4,14 @@ apt update
 apt -y install build-essential gfortran
 
 #Microsoft R Open 3.4.2
-if [ ! -e '/vagrant/microsoft-r-open-3.4.2.tar.gz' ]; then
+if [ ! -e '/vagrant/microsoft-r-open-3.4.3.tar.gz' ]; then
   cd /vagrant
-  wget -q https://mran.blob.core.windows.net/install/mro/3.4.2/microsoft-r-open-3.4.2.tar.gz
+  wget -q https://mran.blob.core.windows.net/install/mro/3.4.3/microsoft-r-open-3.4.3.tar.gz
 fi
-tar zxf /vagrant/microsoft-r-open-3.4.2.tar.gz
+tar zxf /vagrant/microsoft-r-open-3.4.3.tar.gz
 cd microsoft-r-open
 ./install.sh -au
 cd ..
 rm -rf microsoft-r-open
 
-chmod -R 777 /opt/microsoft/ropen/3.4.2/lib64
+chmod -R 777 /opt/microsoft/ropen/3.4.3/lib64
